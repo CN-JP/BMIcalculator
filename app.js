fae3da03c04c4f4b51398e3776bc71fbd0a2875c
Nonso Johnpaul Akamnonu <osnon.nonso@gmail.com>
2022-06-06 14:11:37 +0100

    BMIcalc v.1.1

--- app.js
+++ app.js
@@ -39,3 +39,54 @@ document.querySelector('.comment').innerHTML = `Result:
 you are <span id ="comment">${status}</span>`;
 
 })
+
+
+
+// BMI info-box
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+var btn4 = document.getElementById('btn4');
+var btn5 = document.getElementById('btn5');
+let slider = document.querySelector('.slider');
+
+btn1.onclick = function() {
+  slider.style.marginLeft = '0px';
+  this.style.background = 'aliceblue';
+  btn2.style.background = 'transparent';
+  btn3.style.background = 'transparent';
+  btn4.style.background = 'transparent';
+  btn5.style.background = 'transparent';
+}
+btn2.onclick = function() {
+  slider.style.marginLeft = '-300px';
+  this.style.background = 'aliceblue';
+  btn1.style.background = 'transparent';
+  btn3.style.background = 'transparent';
+  btn4.style.background = 'transparent';
+  btn5.style.background = 'transparent';
+}
+btn3.onclick = function() {
+  slider.style.marginLeft = '-600px';
+  this.style.background = 'aliceblue';
+  btn1.style.background = 'transparent';
+  btn2.style.background = 'transparent';
+  btn4.style.background = 'transparent';
+  btn5.style.background = 'transparent';
+}
+btn4.onclick = function() {
+  slider.style.marginLeft = '-900px';
+  this.style.background = 'aliceblue';
+  btn1.style.background = 'transparent';
+  btn2.style.background = 'transparent';
+  btn3.style.background = 'transparent';
+  btn5.style.background = 'transparent';
+}
+btn5.onclick = function() {
+  slider.style.marginLeft = '-1200px';
+  this.style.background = 'aliceblue';
+  btn1.style.background = 'transparent';
+  btn2.style.background = 'transparent';
+  btn3.style.background = 'transparent';
+  btn4.style.background = 'transparent';
+}
